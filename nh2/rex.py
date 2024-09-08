910648ac8cacc6031609835b9a1fcec256087b89
Daniel Reed <nmlorg@gmail.com>
2024-09-08 11:55:25 -0700

    Set content-type: ...; charset=utf-8 if a [non-empty] str is passed in as a Request's body.
    (The reverse should probably also be done in Response.)
    
    Make the content-type header's structure available as Request/Response.contenttype.
    
    This is stepping into the realm of logic/behavior I don't want to manage/be responsible for 🙁. Ideally I'd use the Request/Response logic from urllib3, requests, etc. and only manage the low-level HTTP/2 I/O in nh2, but that's not obviously feasible.

--- nh2/rex.py
+++ nh2/rex.py
@@ -1,6 +1,25 @@
 """HTTP/2 requests and responses."""
 
 
+class ContentType:
+    def __init__(self, value=''):
+        pieces = [piece.strip() for piece in value.split(';')]
+        self.mediatype = pieces and pieces.pop(0) or None
+        params = dict(piece.split('=', 1) for piece in pieces)
+        self.charset = params.get('charset')
+        self.boundary = params.get('boundary')
+
+    def __str__(self):
+        if not self.mediatype:
+            return ''
+        pieces = [self.mediatype]
+        if self.charset:
+            pieces.append(f'charset={self.charset}')
+        if self.boundary:
+            pieces.append(f'boundary={self.boundary}')
+        return '; '.join(pieces)
+
+
 class Request:
     """An HTTP/2 request."""
 
@@ -15,8 +34,15 @@ class Request:
             ':scheme': 'https',
         }
         self.headers.update(headers)
-        if isinstance(body, str):
-            body = body.encode('utf8')
+        self.contenttype = ContentType(self.headers.get('content-type', ''))
+        if body and isinstance(body, str):
+            assert self.contenttype.charset is None
+            if self.contenttype.mediatype is None:
+                self.contenttype.mediatype = 'text/plain'
+            self.contenttype.charset = 'utf-8'
+            body = body.encode('utf-8')
+        if self.contenttype.mediatype:
+            self.headers['content-type'] = str(self.contenttype)
         self.body = body or b''
 
 
@@ -27,4 +53,5 @@ class Response:
         self.request = request
         self.headers = headers
         self.status = int(headers[':status'])
+        self.contenttype = ContentType(headers.get('content-type', ''))
         self.body = body
--- nh2/test_rex.py
+++ nh2/test_rex.py
@@ -3,8 +3,30 @@
 import nh2.rex
 
 
+def test_contenttype():
+    """Basic ContentType functionality."""
+
+    contenttype = nh2.rex.ContentType('')
+    assert contenttype.mediatype is None
+    assert contenttype.charset is None
+    assert contenttype.boundary is None
+    assert str(contenttype) == ''
+
+    contenttype = nh2.rex.ContentType('text/plain')
+    assert contenttype.mediatype == 'text/plain'
+    assert contenttype.charset is None
+    assert contenttype.boundary is None
+    assert str(contenttype) == 'text/plain'
+
+    contenttype = nh2.rex.ContentType('text/plain;  charset=utf-8')
+    assert contenttype.mediatype == 'text/plain'
+    assert contenttype.charset == 'utf-8'
+    assert contenttype.boundary is None
+    assert str(contenttype) == 'text/plain; charset=utf-8'
+
+
 def test_request_simple():
-    """Basic functionality."""
+    """Basic Request functionality."""
 
     request = nh2.rex.Request('PUT', 'example.com', '/test', (), '')
     assert request.method == 'PUT'
@@ -16,15 +38,37 @@ def test_request_simple():
         ':authority': 'example.com',
         ':scheme': 'https',
     }
+    assert request.contenttype.mediatype is None
+    assert request.contenttype.charset is None
+    assert request.contenttype.boundary is None
     assert request.body == b''
+
+    request = nh2.rex.Request('PUT', 'example.com', '/test', (), '\u2022')
+    assert request.method == 'PUT'
+    assert request.host == 'example.com'
+    assert request.path == '/test'
+    assert request.headers == {
+        ':method': 'PUT',
+        ':path': '/test',
+        ':authority': 'example.com',
+        ':scheme': 'https',
+        'content-type': 'text/plain; charset=utf-8',
+    }
+    assert request.contenttype.mediatype == 'text/plain'
+    assert request.contenttype.charset == 'utf-8'
+    assert request.contenttype.boundary is None
+    assert request.body == b'\xe2\x80\xa2'
 
 
 def test_response_simple():
-    """Basic functionality."""
+    """Basic Response functionality."""
 
     request = nh2.rex.Request('PUT', 'example.com', '/test', (), '')
     response = nh2.rex.Response(request, {':status': '200'}, b'')
     assert response.request is request
     assert response.headers == {':status': '200'}
     assert response.status == 200
+    assert response.contenttype.mediatype is None
+    assert response.contenttype.charset is None
+    assert response.contenttype.boundary is None
     assert response.body == b''
