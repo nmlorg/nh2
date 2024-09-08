1b203fcac9631f1d1cb7e8f4607865f2af380998
Daniel Reed <nmlorg@gmail.com>
2024-09-07 19:33:36 -0700

    Support multiple requests per Connection.
    Connection.read() now returns a list of completed Responses (if any); it must be called repeatedly until a Request you're interested in appears.

--- nh2/connection.py
+++ nh2/connection.py
@@ -24,6 +24,8 @@ class Connection:
         self.c.initiate_connection()
         self.flush()
 
+        self.streams = {}
+
     def request(self, method, path):
         """Send a method request for path."""
 
@@ -32,36 +34,35 @@ class Connection:
     def send(self, request):
         """Send the given Request."""
 
-        self.c.send_headers(1, request.headers, end_stream=True)
+        return LiveRequest(self, request)
+
+    def new_stream(self, live_request):
+        """Reserve a new stream_id and begin tracking the given LiveRequest."""
+
+        stream_id = self.c.get_next_available_stream_id()
+        self.streams[stream_id] = live_request
+        return stream_id
+
+    def read(self):
+        """Wait until data is available. Return a list of Responses finalized by that data."""
+
+        data = self.s.recv(65536 * 1024)
+        if not data:
+            return ()
+
+        responses = []
+        for event in self.c.receive_data(data):
+            print(event)
+            if isinstance(event, h2.events.DataReceived):
+                # Update flow control so the server doesn't starve us.
+                self.c.acknowledge_received_data(event.flow_controlled_length, event.stream_id)
+                live_request = self.streams[event.stream_id]
+                live_request.receive(event.data)
+            elif isinstance(event, h2.events.StreamEnded):
+                live_request = self.streams.pop(event.stream_id)
+                responses.append(live_request.ended())
         self.flush()
-
-    def read(self):  # pylint: disable=missing-function-docstring
-        body = b''
-        response_stream_ended = False
-        while not response_stream_ended:
-            # read raw data from the socket
-            data = self.s.recv(65536 * 1024)
-            if not data:
-                break
-
-            # feed raw data into h2, and process resulting events
-            events = self.c.receive_data(data)
-            for event in events:
-                print(event)
-                if isinstance(event, h2.events.DataReceived):
-                    # update flow control so the server doesn't starve us
-                    self.c.acknowledge_received_data(event.flow_controlled_length, event.stream_id)
-                    # more response body data received
-                    body += event.data
-                if isinstance(event, h2.events.StreamEnded):
-                    # response body completed, let's exit the loop
-                    response_stream_ended = True
-                    break
-            self.flush()
-
-        print("Response fully received:")
-        print(body.decode())
-        return body.decode()
+        return responses
 
     def flush(self):
         """Send any pending data to the server."""
@@ -89,3 +90,38 @@ class Request:
             (':authority', host),
             (':scheme', 'https'),
         ]
+
+
+class Response:
+    """A response."""
+
+    def __init__(self, request, body):
+        self.request = request
+        self.body = body
+
+
+class LiveRequest:
+    """A Request that's been sent over a Connection that hasn't received a StreamEnded yet."""
+
+    def __init__(self, connection, request):
+        self.connection = connection
+        self.stream_id = connection.new_stream(self)
+        self.request = request
+        self.received = []
+        self.send_headers()
+
+    def send_headers(self):
+        """Send the request's headers."""
+
+        self.connection.c.send_headers(self.stream_id, self.request.headers, end_stream=True)
+        self.connection.flush()
+
+    def receive(self, data):
+        """Store data received by a DataReceived."""
+
+        self.received.append(data)
+
+    def ended(self):
+        """Mark the request as being finalized."""
+
+        return Response(self.request, b''.join(self.received).decode('utf8'))
--- nh2/test_connection.py
+++ nh2/test_connection.py
@@ -1,14 +1,31 @@
 """Tests for nh2.connection."""
 
+import json
+
 from nh2 import connection
 
 
 def test_simple():
     """Basic functionality."""
 
-    conn = connection.Connection('http2.golang.org', 443)
+    conn = connection.Connection('httpbin.org', 443)
     try:
-        conn.request('GET', '/reqinfo')
-        assert conn.read() == '<a href="https://go.dev/reqinfo">Found</a>.\n\n'
+        conn.request('GET', '/get?a=b')
+        responses = None
+        while not responses:
+            responses = conn.read()
+        assert conn.streams == {}
+        assert len(responses) == 1
+        response = json.loads(responses[0].body)
+        assert response['args'] == {'a': 'b'}
+
+        conn.request('GET', '/get?c=d')
+        responses = None
+        while not responses:
+            responses = conn.read()
+        assert conn.streams == {}
+        assert len(responses) == 1
+        response = json.loads(responses[0].body)
+        assert response['args'] == {'c': 'd'}
     finally:
         conn.close()
